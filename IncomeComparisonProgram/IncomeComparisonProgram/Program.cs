e90c64f45164954e2045b2f65982d97057d81b86
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-01-07 13:45:31 -0800

    Created Salary Comparison Program

---
+++ IncomeComparisonProgram/IncomeComparisonProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparisonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1\nWhat is the hourly rate?");
+            int hourlyRate1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours worked per week?");
+            int hoursWorked1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2\nWhat is the hourly rate?");
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many hours worked per week?");
+            int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
+
+            int salaryPerson1 = hourlyRate1 * hoursWorked1 * 52;
+            int salaryPerson2 = hourlyRate2 * hoursWorked2 * 52;
+
+            Console.WriteLine("Annual salary of Person 1: " + salaryPerson1);
+            Console.WriteLine("Annual salary of Person 2: " + salaryPerson2);
+
+            bool greaterSalary = salaryPerson1 > salaryPerson2;
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?\n" + greaterSalary);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minues five  = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 10 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalce = 12 > 5;
+            //Console.WriteLine(trueOrFalce.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature >= roomTemperature;
+            //Console.WriteLine(isWarm.ToString());
+            //Console.ReadLine();
+
+            int userInput = Convert.ToInt32(Console.ReadLine()) * 50;
+
+            int secondUserInput = Convert.ToInt32(Console.ReadLine()) + 25;
+            Console.WriteLine(secondUserInput);
+
+
+
+
+        }
+    }
+}
