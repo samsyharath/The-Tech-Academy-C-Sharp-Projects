76101d0f3ce46bb6bf1f12772991b8a0bceec89d
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-01-07 14:43:10 -0800

    Car Insurance Qualification Program

---
+++ CarInsuranceQualification/CarInsuranceQualification/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceQualification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? \"true\" or \"false\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many Speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = age > 15 && dui == false && speedingTickets < 3;
+
+            Console.WriteLine("qualified?: " + qualified);
+
+        }
+    }
+}
