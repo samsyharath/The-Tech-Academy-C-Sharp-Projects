a675070e7c63af4c5251797f4d0b14f6b709319d
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-01-16 22:25:22 -0800

    Created program that adds, multiplies and subtracts

--- MathMethods/MathMethods/Maths.cs
+++ MathMethods/MathMethods/Maths.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace MathMethods
 {
-    class Maths
+    public class Maths
     {
+        public int Sum(int num1)
+        {
+            int total;
+            total = num1 + 10;
+            return total;
+        }
+
+        public int Sum(decimal num2)
+        {
+            int total = Convert.ToInt32(num2 * 100);
+            return total;
+        }
+        public int Sum(string num3)
+        {
+            try
+            {
+                int total = Convert.ToInt32(num3) - 1;
+                return total;
+            }
+            catch (FormatException ex)
+            {
+                
+                Console.WriteLine("That is not a number");
+                return 0;
+            }
+        }
     }
 }
--- MathMethods/MathMethods/Program.cs
+++ MathMethods/MathMethods/Program.cs
@@ -10,6 +10,24 @@ namespace MathMethods
     {
         static void Main(string[] args)
         {
+            Maths math = new Maths();
+
+            Console.WriteLine("Please submit an integer.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int total= math.Sum(num1);
+            Console.WriteLine(total);
+
+            Console.WriteLine("Please submit a decimal.");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+            total = math.Sum(num2);
+            Console.WriteLine(total);
+
+
+            Console.WriteLine("Please submit an string.");
+            string num3 = Console.ReadLine();
+            total = math.Sum(num3);
+            Console.WriteLine(total);            
+            Console.ReadLine();
         }
     }
 }
