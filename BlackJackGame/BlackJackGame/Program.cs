6e8d4826a09058a5fd824de92c4266a34a6e344c
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-01-18 01:54:15 -0800

    Operation overload to add and remove players

--- BlackJackGame/BlackJackGame/Program.cs
+++ BlackJackGame/BlackJackGame/Program.cs
@@ -10,23 +10,26 @@ namespace BlackJackGame
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Sam", "Joe" };
-            game.ListPlayers();
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            //game.Players = new List<string>() { "Jesse", "Sam", "Joe" };
+            //game.ListPlayers();
 
-            //game.Play();
-            Console.ReadLine();
-
-
-            //Deck deck = new Deck();
-            //int timesShuffled = 0;
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{ Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
+            ////game.Play();
             //Console.ReadLine();
+
+            Player player = new Player();
+            player.Name = "Jesse";
+            game += player;
+            game = game - player;
+            Deck deck = new Deck();
+            deck.Shuffle();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
         
