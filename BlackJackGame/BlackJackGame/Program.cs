b5917be7c48a28a2740c02937321a95254dca701
Sam Syharath <112458977+samsyharath@users.noreply.github.com>
2024-01-17 23:13:37 -0800

    Created Inheriting class TwentyOneGame

--- BlackJackGame/BlackJackGame/Program.cs
+++ BlackJackGame/BlackJackGame/Program.cs
@@ -10,15 +10,22 @@ namespace BlackJackGame
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck();
-            int timesShuffled = 0;
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
-            { Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Sam", "Joe" };
+            game.ListPlayers();
+            game.Play();
             Console.ReadLine();
+
+
+            //Deck deck = new Deck();
+            //int timesShuffled = 0;
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{ Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
 
         
